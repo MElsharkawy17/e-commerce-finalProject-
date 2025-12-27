109e7dcc77b4408c435ea5b0b6da6f7603e4b0cf
Malak Elsharkawy <elsharkawymalak764@gmail.com>
2025-12-27 04:14:24 +0200

    Fix hero.js path issue

--- src/Services/AuthServices.js
+++ src/Services/AuthServices.js
@@ -1,55 +1,16 @@
-// // AuthServices.js
-
-// export async function login(data) {
-//   try {
-//     const res = await fetch("https://ecommerce.routemisr.com/api/v1/auth/signin", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify(data),
-//     });
-//     if (!res.ok) {
-//       const err = await res.json();
-//       throw new Error(err.error || `Status: ${res.status}`);
-//     }
-//     return await res.json();
-//   } catch (error) {
-//     console.error("Login Error:", error);
-//     return { error: error.message };
-//   }
-// }
-
-// export async function signUp(data) {
-//   try {
-//     const res = await fetch("https://ecommerce.routemisr.com/api/v1/auth/signup", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify(data),
-//     });
-//     if (!res.ok) {
-//       const err = await res.json();
-//       throw new Error(err.error || `Status: ${res.status}`);
-//     }
-//     return await res.json();
-//   } catch (error) {
-//     console.error("SignUp Error:", error);
-//     return { error: error.message };
-//   }
-// }
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// AuthServices.js
 
 export async function login(data) {
   try {
-    const res = await fetch(`${BASE_URL}/auth/signin`, {
+    const res = await fetch("https://ecommerce.routemisr.com/api/v1/auth/signin", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     });
-
     if (!res.ok) {
       const err = await res.json();
       throw new Error(err.error || `Status: ${res.status}`);
     }
-
     return await res.json();
   } catch (error) {
     console.error("Login Error:", error);
@@ -59,17 +20,15 @@ export async function login(data) {
 
 export async function signUp(data) {
   try {
-    const res = await fetch(`${BASE_URL}/auth/signup`, {
+    const res = await fetch("https://ecommerce.routemisr.com/api/v1/auth/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     });
-
     if (!res.ok) {
       const err = await res.json();
       throw new Error(err.error || `Status: ${res.status}`);
     }
-
     return await res.json();
   } catch (error) {
     console.error("SignUp Error:", error);
